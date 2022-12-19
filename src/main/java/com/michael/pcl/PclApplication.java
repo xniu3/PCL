d1ae67886279b9a710de3b5d7585acdc91671a55
Xinhao.Niu <xniu3@ualberta.ca>
2022-12-19 09:26:31 -0800

    First Commit

---
+++ src/main/java/com/michael/pcl/PclApplication.java
@@ -0,0 +1,13 @@
+package com.michael.pcl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PclApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PclApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/michael/pcl/PclApplicationTests.java
@@ -0,0 +1,13 @@
+package com.michael.pcl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PclApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
